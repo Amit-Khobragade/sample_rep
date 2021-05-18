c6ebeb018946bfd4923977962f332efe2057cedb
amit khobragade <amitkh1102@outlook.com>
2021-05-18 12:51:23 +0530

    created scene1

---
+++ src/Control1.java
@@ -0,0 +1,14 @@
+package com.controls;
+
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+
+public class Control1 {
+	public void store() {
+		System.out.println( "hii" );
+	}
+	@FXML
+	private TextField name;
+	@FXML
+	private TextField venue;
+}
